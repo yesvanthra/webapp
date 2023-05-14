2513a9acb454c54866baf45802c6830edb59293f
yesvanthra <132344842+yesvanthra@users.noreply.github.com>
2023-05-14 16:26:26 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,2 +1,30 @@
-print("hello world")
-print("hi everyone")
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+# test_app.py
+import unittest
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
+
