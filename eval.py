80d146021c53d445b1839f5cfb12db092131201a
yesvanthra <132344842+yesvanthra@users.noreply.github.com>
2023-05-15 09:38:27 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-# app.py
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,20 +11,5 @@ if __name__ == '__main__':
     app.run()
 
 
-# test_app.py
-import unittest
-from app import app
 
-class AppTestCase(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
-
-    def test_hello_world(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
-
-if __name__ == '__main__':
-    unittest.main()
 
