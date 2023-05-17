25adb87f4e1193e72001732e8e5f5b81eee7219b
yesvanthra <132344842+yesvanthra@users.noreply.github.com>
2023-05-17 15:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
+    logging.basicConfig(level=logging.DEBUG)  # Set the desired logging level (e.g., DEBUG, INFO, WARNING, ERROR)
+    logger = logging.getLogger(__name__)
+    logger.info("Your log message")  # for standard output
 if __name__ == '__main__':
     app.run()
 
