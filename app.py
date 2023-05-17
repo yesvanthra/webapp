d6e9f54bfa92f8ceb063b6e8994dbfafd05d3d09
yesvanthra <132344842+yesvanthra@users.noreply.github.com>
2023-05-17 17:22:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
+# Create a logger instance
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create a file handler and set the logging level
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+
+# Create a log formatter and set it for the file handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
+
+
 @app.route('/')
 def hello_world():
-    return'Hello,World!'
+    logger.info('Hello, World!')  # Log the message instead of printing
+    return 'Hello, World!'
+
+
 if __name__ == '__main__':
     app.run()
 
